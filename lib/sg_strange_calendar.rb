a5d70f6bfe242bf81dd7b9f3e029aa6ec012b07b
Yusuke Hirono <yusuke.hirono.j7n@jp.denso.com>
2025-01-20 01:41:58 +0900

    Lv2実装

--- lib/sg_strange_calendar.rb
+++ lib/sg_strange_calendar.rb
@@ -24,7 +24,10 @@ class SgStrangeCalendar
   end
 
   def generate(vertical: false)
-    # write your code here
+    # 強調する日付がある場合、その日付に@をつけておく
+    if @today_year && @today_year == @year
+      @calender[@today_month - 1][@today_day - 1] = '@' + @calender[@today_month - 1][@today_day - 1].to_s
+    end
     # 各月に空文字を追加して、各月の日付を整形
     formatted_calender = @calender.map.with_index do |month, index|
       # 各月の先頭に空文字を追加
@@ -39,6 +42,12 @@ class SgStrangeCalendar
     month_header = [@year.to_s] + MONTHS.map { |month| month.ljust(@year.to_s.length) }
 
     # 曜日ヘッダーを先頭に挿入。transposeして月ヘッダーを先頭に挿入。再度transposeし、各日付を空白で結合してから１つの文字列に改行コードで結合する
-    formatted_calender.unshift(DAYS).transpose.unshift(month_header).transpose.map { |sub_array| sub_array.join(' ').rstrip }.join("\n")
+    output = formatted_calender.unshift(DAYS).transpose.unshift(month_header).transpose.map { |sub_array| sub_array.join(' ').rstrip }.join("\n")
+    if @today_year && @today_year == @year
+      # 強調する日付の@を[]に変換
+      output = output.gsub(" @#{@today_day} ", @today_day < 10 ? " [#{@today_day}]" : "[#{@today_day}]") # 1桁の場合は空白を追加
+                     .gsub(" @#{@today_day}", "[#{@today_day}]") # 各月の最後は文字の後ろにスペースがないので別途処理
+    end
+    output
   end
 end
